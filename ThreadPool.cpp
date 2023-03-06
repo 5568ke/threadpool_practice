801a15475417a5933d7282a7f2cc478ca5144925
clown911026 <5568keb67595@gmail.com>
2023-03-06 17:05:29 +0800

    basic version of ThreadPool with following feature:   1 submit task by pool.enqueue()   2 get job return value with future object   3 per thread job queue   4 print log message of use time (task numbers , job size can be decide)
      bug:
        job stealing is with bug currently

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -1,4 +1,6 @@
 #include <thread>
+#include <chrono>
+#include <string>
 #include <iostream>
 #include <vector>
 #include <functional>
@@ -14,14 +16,19 @@ public:
     }
 
     ~ThreadPool() {
-        stop();
+        // stop();
     }
 
     template<typename F, typename... Args>
-    void enqueue(F&& f, Args&&... args) {
-        auto task = std::make_shared<std::packaged_task<void()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+    decltype(auto) enqueue(F&& f, Args&&... args) {
+        using return_type = typename std::result_of<F(Args...)>::type;
+        auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+        std::future<return_type> res = task->get_future(); 
+
+        // add job to per thread job queue
         auto index = next_queue_.fetch_add(1) % queues_.size();
         queues_[index].enqueue([task]() { (*task)(); });
+        return res;
     }
 
     void stop() {
@@ -49,13 +56,16 @@ private:
                 task();
             }
             else {
+                // bug : job stealing now cause std::terminate()
+
                 // Steal task from other queues
-                for (size_t i = 0; i < queues_.size(); ++i) {
-                    if (i != index && queues_[i].try_dequeue(task)) {
-                        task();
-                        break;
-                    }
-                }
+                // for (size_t i = 0; i < queues_.size(); ++i) {
+                //     if (i != index && queues_[i].try_dequeue(task)) {
+                //         task();
+                //         break;
+                //     }
+                // }
+        
                 // Sleep for a while to avoid spinning
                 std::this_thread::yield();
             }
@@ -69,8 +79,47 @@ private:
 
 
 int main(){
-  const int thread_count=std::thread::hardware_concurrency();
-  ThreadPool pool(4);
-  pool.stop();
+  int thread_count,task_size; //=std::thread::hardware_concurrency();
+  std::cout<<"threads number : ";
+  std::cin>>thread_count;
+  std::cout<<std::endl<<"task size : ";
+  std::cin>>task_size;
+  std::cout<<std::endl;
 
+  std::string output_data="";
+  ThreadPool pool(thread_count);
+  output_data+="====================================";
+  output_data+='\n';
+  output_data+=" initialize threadpool with : ";
+  output_data+= std::to_string(thread_count);
+  output_data+=" threads";
+  output_data+='\n'; 
+  output_data+=" task size : ";
+  output_data+= std::to_string(task_size);
+  output_data+='\n';
+
+
+  std::vector<std::future<int>> futures(1000000);
+
+    
+  std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+  for(int i=0;i<1000000;i++){
+      futures[i]=pool.enqueue([=](){
+          int c=0;
+          for(int count=0;count<task_size;count++){
+            c+=count;
+          }
+          return c;
+      });
+  }
+  pool.stop();
+  std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+  output_data+=" use time : ";
+  output_data+=std::to_string((t2-t1).count());
+  output_data+='\n';
+  output_data+=" exit";
+  output_data+='\n';
+  output_data+="====================================";
+  output_data+='\n';
+  std::cout<<output_data;
 }
