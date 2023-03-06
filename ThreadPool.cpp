3509d63fe853dbf552cb80459a7c4a74b6523133
clown911026 <5568keb67595@gmail.com>
2023-03-06 23:17:43 +0800

    change insult_of to invoke_result (c++20 removed result_of)

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -21,7 +21,7 @@ public:
 
     template<typename F, typename... Args>
     decltype(auto) enqueue(F&& f, Args&&... args) {
-        using return_type = typename std::result_of<F(Args...)>::type;
+        using return_type = typename std::invoke_result<F,Args...>::type;
         auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
         std::future<return_type> res = task->get_future(); 
 
